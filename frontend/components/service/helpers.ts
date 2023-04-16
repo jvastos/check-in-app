dbcca6081955b5965e5fb1f5c616638dbeed924b
João Almeida <jvastos@gmail.com>
2023-04-16 20:50:01 +0200

    Sets the production DB url to the Android development build.

--- frontend/components/service/helpers.ts
+++ frontend/components/service/helpers.ts
@@ -1,8 +1,12 @@
 import { REACT_APP_API_URL_DEV, REACT_APP_API_URL } from '@env';
+import { Platform } from 'react-native';
 
 const chooseAPI_URL = (environment: string | undefined) => {
 	switch (environment) {
 		case 'development':
+			if (Platform.OS === 'android') {
+				return REACT_APP_API_URL;
+			}
 			return REACT_APP_API_URL_DEV;
 		case 'test':
 			return REACT_APP_API_URL_DEV;
